3a1d72aa6d060a57f7e6fee92f13908ea61cd4b5
clehider <eduarddring@gmail.com>
2025-02-17 11:02:40 -0400

    PRIMER DESPLIEGUE EXITOSO CON VUE TODO OK

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAe3GGXpU-PoeYhCZyrQ_o7i4ozipu25L8",
   authDomain: "webcoonsbolivia.firebaseapp.com",
+  databaseURL: "https://webcoonsbolivia-default-rtdb.firebaseio.com",
   projectId: "webcoonsbolivia",
   storageBucket: "webcoonsbolivia.firebasestorage.app",
   messagingSenderId: "495512695633",
   appId: "1:495512695633:web:da3fc06dbf212046651970"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const rtdb = getDatabase(app);
 
-export default app;
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
